4ff3512aabe4dfd20d95dffdbfe835c6e8a84ca8
Marco <mbarozzi@gmail.com>
2024-07-16 02:32:59 -0300

    requisições http via getx

---
+++ vidflow/lib/services/videos_api.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+import 'package:vidflow/model/video.dart';
+import 'package:vidflow/utils/api_endpoints.dart';
+
+class VideosApi extends GetConnect{
+  @override
+  void onInit(){
+    super.onInit();
+    httpClient.baseUrl = ApiEndpoints.baseUrl;
+  }
+
+  Future<Response<void>> create(Video video,String token)=>post(ApiEndpoints.addVideosByUser,
+  video.toMap(),headers: {"Content-Type":"application/json","Authorization":"Bearer $token" });
+
+  Future<Response<List<Video>>> getAllFronUser(userId)=> get(ApiEndpoints.videos,
+  decoder: Video.listFromJson, query:{"userId":userId.toString()});
+}
