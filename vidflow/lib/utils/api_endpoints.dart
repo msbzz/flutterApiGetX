2bce7004092cc1839984353520f4edf82df6b59b
Marco <mbarozzi@gmail.com>
2024-07-16 00:09:45 -0300

    realizando registro do usuario

--- vidflow/lib/utils/api_endpoints.dart
+++ vidflow/lib/utils/api_endpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints{
-  static const String baseUrl="http://localhost:3000/";
+  static const String baseUrl="http://192.168.0.64:3000/";
   static const String register =  "register";
   static const String login =  "login";
   static const String videos =  "videos";
